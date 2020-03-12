3cd83f58b2932bc1d821d293c6591c699164b858
YY-0123 <2579439253@qq.com>
2020-03-12 10:11:15 +0800

    主页-轮播图

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,40 +1,52 @@
 <template>
-	<view class="container">
-	<view class="search">
-		<view class="input-box">
-		<input type="text" />
-	</view>
-	<view class="content"></view>
-	</view>
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view class="container">
+    <!-- 搜索栏 -->
+    <view class="search">
+      <view class="input-box">
+        <input type="text" />
+      </view>
+      <view class="content"></view>
+    </view>
+    <!-- 轮播图 -->
+    <swiper indicator-dots>
+      <swiper-item>
+        <image src="/static/uploads/banner1.png" />
+      </swiper-item>
+      <swiper-item>
+        <image src="/static/uploads/banner2.png" />
+      </swiper-item>
+      <swiper-item>
+        <image src="/static/uploads/banner3.png" />
+      </swiper-item>
+    </swiper>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Nihao'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+  data() {
+    return {
+      title: "Nihao"
+    };
+  },
+  onLoad() {},
+  methods: {}
+};
 </script>
 
 <style>
-	.search .input-box {
-		padding: 20rpx 16rpx;
-		background-color: #FF2D4A;
-	}
-	.search .input-box input {
-		background-color: #fff;
-	}
+.search .input-box {
+  padding: 20rpx 16rpx;
+  background-color: #ff2d4a;
+}
+.search .input-box input {
+  background-color: #fff;
+}
+swiper {
+  height: 340rpx;
+}
+swiper image {
+  width: 750rpx;
+  height: 340rpx;
+}
 </style>
