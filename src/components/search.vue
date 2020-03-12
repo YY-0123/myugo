21a2bdb2b8502ac1b012cddc2e6a3992e3f02266
YY-0123 <2579439253@qq.com>
2020-03-12 15:30:27 +0800

    输入框获取焦点背景变色

---
+++ src/components/search.vue
@@ -0,0 +1,43 @@
+<template>
+  <view class="search" :class='{focused: isFocused}'>
+    <!-- 搜索框 -->
+    <view class="input-box">
+      <input type="text" @focus="goSearch"/>
+    </view>
+    <!-- 搜索结果 -->
+    <view class="content"></view>
+  </view>
+</template>
+<script>
+export default {
+  data() {
+    return {
+        isFocused: false
+    };
+  },
+   methods: {
+    goSearch () {
+      console.log('hello')
+      // 当输入框获取焦点时，在父元素添加一个类名 focused
+      this.isFocused = true
+    }
+  }
+};
+</script>
+<style lang='less'>
+.search {
+  .input-box {
+    background-color: #eb4450;
+    padding: 20rpx;
+    input {
+      background-color: #fff;
+      border-radius: 6rpx;
+    }
+  }
+  &.focused {
+    .input-box {
+      background-color: #eee;
+    }
+  }
+}
+</style>    
